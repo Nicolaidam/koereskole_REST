46c570528feb84bbaa7b951dd4fc92c4ce3037fd
Nicolai Dam <nicolaidam96@gmail.com>
2019-04-30 13:26:52 +0200

    h

---
+++ src/java/server/Koreskole.java
@@ -0,0 +1,51 @@
+package server;
+
+import java.io.Serializable;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+public class Koreskole implements Serializable {
+	public Koreskole() throws Exception {
+		// TODO Auto-generated constructor stub
+	}
+
+	static final long serialVersionUID = 1234567999;
+
+	String id;
+	String navn;
+	String adresse;
+	int postnummer;
+	int telefonnummer;
+	String mail;
+
+
+
+	@Override
+	public String toString() {
+		String s = "";
+
+		s+="id=";
+		s+= id;
+
+		s+="\nnavn=";
+		s+=navn;
+
+		s+="\nadresse=";
+		s+=adresse;
+
+		s+="\npostnummer=";
+		s+=postnummer;
+
+		s+="\ntelefonnummer=";
+		s+=telefonnummer;
+
+		s+="\nmail=";
+		s+=mail;
+
+
+		return s;
+	}
+
+
+
+}
