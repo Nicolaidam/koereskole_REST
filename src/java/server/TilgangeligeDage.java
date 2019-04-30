c40358df1e528dd4d63362d8b8b529e91a6f5784
Nicolai Dam <nicolaidam96@gmail.com>
2019-04-30 13:26:24 +0200

    h

---
+++ src/java/server/TilgangeligeDage.java
@@ -0,0 +1,18 @@
+package server;
+
+import java.io.Serializable;
+
+public class TilgangeligeDage implements Serializable {
+	
+	static final long serialVersionUID = 1234567891;
+
+	
+	int tilgangelig_mandag;
+	int tilgangelig_tirsdag;
+	int tilgangelig_onsdag;
+	int tilgangelig_torsdag;
+	int tilgangelig_fredag;
+	int tilgangelig_lordag;
+	int tilgangelig_sondag;
+
+}
