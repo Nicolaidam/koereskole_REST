9f79b7e8de471477292b5b06519c0b5fe3bbfce0
Nicolai Dam <nicolaidam96@gmail.com>
2019-04-30 13:26:38 +0200

    h

--- src/java/server/KøreskolePriserInterface.java
+++ src/java/server/KøreskolePriserInterface.java
@@ -9,7 +9,7 @@ public interface KøreskolePriserInterface extends java.rmi.Remote {
 	boolean logIndAdmin(String adminBrugernavn, String adminKodeord) 				throws java.rmi.RemoteException;
 	boolean sletKøreskole(String adminBrugernavn, String adminKodeord, String køreskole_id)         throws java.rmi.RemoteException;
 	boolean sletKøreskoleTilbud(String adminBrugernavn, String adminKodeord, int tilbud_id )	throws java.rmi.RemoteException;
-	boolean opretKøreskoleAdmin(String adminBrugernavn, String adminKodeord, Køreskole køreskole) 	throws java.rmi.RemoteException;
+	boolean opretKøreskoleAdmin(String adminBrugernavn, String adminKodeord, Koreskole køreskole) 	throws java.rmi.RemoteException;
 	String getAlleKøreskoler(String adminBrugernavn, String adminKodeord)				throws java.rmi.RemoteException;
 
 	
@@ -20,7 +20,7 @@ public interface KøreskolePriserInterface extends java.rmi.Remote {
 	boolean sletTilbud(String brugernavn, String kodeord, int[] tilbudID)				throws java.rmi.RemoteException;
 	String getTilbudKøreskole(String brugernavn, String kodeord)					throws java.rmi.RemoteException;
 	String getKøreskole(String brugernavn, String kodeord)						throws java.rmi.RemoteException;
-	boolean opretKøreskole(String brugernavn, String kodeord, Køreskole køreskole) 			throws java.rmi.RemoteException;
+	boolean opretKøreskole(String brugernavn, String kodeord, Koreskole køreskole) 			throws java.rmi.RemoteException;
 
 	//----------køreskoleelever--------//
 	String getTilbudFraPostnummer(int postnummer)							throws java.rmi.RemoteException;
